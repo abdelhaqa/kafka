e8d479f8606f17a8ff0aab46c2de2d02b3e04cc9
Kumar <vk83@INMAC111AH.local>
2017-11-11 17:22:09 +0530

    removed unused classes

--- src/main/java/raiskumar/com/github/producer/ProducerClient.java
+++
@@ -1,8 +0,0 @@
-package raiskumar.com.github.producer;
-
-public class ProducerClient {
-    public static void main(String[] args){
-        EventPublisher ep = new EventPublisher();
-        ep.publish("test", "test message !");
-    }
-}
